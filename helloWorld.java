9ebb3171e8c67a7cd9bcd2294f516526f135a8fd
Serkan Hatipoglu <hserkan7@yahoo.com>
2018-07-07 13:57:18 -0700

    m1

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,14 @@
-public class helloWorld {
-
+public class HelloWorld {
+    public String makeSentence(String[] words) {
+         StringBuffer sentence = new StringBuffer();
+         for (String w : words) sentence.append(w);
+         return sentence.toString();
+        }
+        
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
 
 }
+ 
